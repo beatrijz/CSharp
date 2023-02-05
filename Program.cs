28420cd99791219f355bd34bc3cc683e92e3d4a4
Beatriz Moura <b3am0ur4@outlook.com>
2023-02-05 15:50:36 -0300

    Exercício de poupanca do alura

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Programa
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Execultando o projeto 10 - Calcula Polpança");
+
+		double investimento = 1000;
+		int mes = 1;
+
+		//rentimento de 0.5% (0.005) ao mês
+		while (mes <= 12)
+		{
+			investimento = investimento + investimento * 0.005;
+			Console.WriteLine("No mês " + mes + " você tem R$" + investimento);
+			// mes += 1;
+			//mes = mes + 1;
+			mes++;
+
+		}
+
+		Console.WriteLine("Tecle enter para fechar");
+		Console.WriteLine();
+	}
+}
